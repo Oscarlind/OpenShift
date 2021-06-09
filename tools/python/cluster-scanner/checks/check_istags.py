83e5ce8455fa7029b98523490e1f73aa14bb44b6
Oscar <olindhol@redhat.com>
2021-06-09 07:58:02 +0200

    Updated istag check to return a dict

--- tools/python/cluster-scanner/checks/check_istags.py
+++ tools/python/cluster-scanner/checks/check_istags.py
@@ -8,16 +8,22 @@ from prettytable import PrettyTable
 
 
 def check_istags(dyn_client):
-    tag_list = []
+    tag_dict = {}
     istag_table = PrettyTable(['Namespace', 'ImageStream', 'Number of tags'])
     v1_istags = dyn_client.resources.get(api_version='image.openshift.io/v1', kind='ImageStream')
     for tag in v1_istags.get().items:
+        if "openshift" in tag.metadata.namespace or 10 >= len(tag.status.tags):
+            pass
+        else:
+            tag_dict[tag.metadata.namespace] = []
+    for tag in v1_istags.get().items:
         if "openshift" in tag.metadata.namespace:
+            pass
+        elif 10 >= len(tag.status.tags):
             pass
         else:
             istag_table.add_row([tag.metadata.namespace, tag.metadata.name, len(tag.status.tags)])
-        tag_list.append(tag.metadata.namespace + tag.metadata.name + str(len(tag.status.tags)))
+            tag_dict[tag.metadata.namespace].append(tag.metadata.name + " " + str(len(tag.status.tags)))
     print("\nNumber of ImageStreamTags used per ImageStream")
     print("\n", istag_table)
-    return tag_list
-        
+    return tag_dict
